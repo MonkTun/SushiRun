199f944c9f08d42ebcb034a45a88a4c821e3eb7a
ornifer <imnotthemano@gmail.com>
2023-12-10 22:31:00 -1000

    square has a level system
    square levels over time, needs sprite model and change square model.

---
+++ Assets/Scripts/PlayerLevelUp.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLevelUp : MonoBehaviour
+{
+    public float Level = 1;
+    public float canLevelUp = 1; //amount of seconds it takes per level up
+    public SpriteRenderer squareColor;
+    
+    void _PlayerLevelUp(float l)
+    {
+        if(Time.time > canLevelUp && Level == l)
+        { 
+            print("level is" + l);
+             // need to add a change of sprite when we get the art for sprites
+            Level = Level + 1;
+            canLevelUp = canLevelUp + 3f; // change float to increase time per level up
+        }
+            
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+            
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _PlayerLevelUp(1);
+        _PlayerLevelUp(2);
+        _PlayerLevelUp(3);
+        _PlayerLevelUp(4);
+        _PlayerLevelUp(5);
+        _PlayerLevelUp(6);
+        _PlayerLevelUp(7);
+        _PlayerLevelUp(8);
+        _PlayerLevelUp(9);
+        _PlayerLevelUp(10);
+
+    }   
+}
