8a8ff91cdfed7461fe7401e8a968f3ee829ecd47
Tundraswan <peroni014@gmail.com>
2023-12-13 12:06:18 -1000

    fixed blurry character, had some fun with obstacles movement and spawning

--- Assets/ObstacleBehavior/Scripts/ObstacleMovement.cs
+++ Assets/ObstacleBehavior/Scripts/ObstacleMovement.cs
@@ -6,17 +6,28 @@ public class ObstacleMovement : MonoBehaviour
 {
     public float speed = 20.0f;
 
-    private float speedincrease = 0.0005f;
+    private float speedincrease = 0.0009f;
+
+    private bool isTrollOne;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        isTrollOne = Random.Range(0, 10f) < 1f;
     }
 
     // Update is called once per frame
     void Update()
     {
+ 
         speed = speed + (float)Time.timeSinceLevelLoad * speedincrease;
+
+        if (isTrollOne && speed > 0 && transform.position.x < -4)
+        {
+            speed = -speed;
+            
+        }
+        
         transform.Translate(Vector2.left * speed * Time.deltaTime);
     }
 }
