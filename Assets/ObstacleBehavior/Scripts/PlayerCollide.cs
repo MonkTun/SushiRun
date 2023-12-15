43b7e7813eac904a5bd6758f86328c31f230c5c8
ian-nishikawa <iannishikawa808@gmail.com>
2023-12-14 16:33:16 -1000

    invicibility tewtss

--- Assets/ObstacleBehavior/Scripts/PlayerCollide.cs
+++ Assets/ObstacleBehavior/Scripts/PlayerCollide.cs
@@ -4,22 +4,19 @@ using UnityEngine;
 
 public class PlayerCollide : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+    private bool isInvincible = false;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Obstacle"))
         {
+            if (isInvincible)
+            {
+                isInvincible = false;
+                Destroy(collision.gameObject);
+                return;
+            }
+            
             GameManager.Instance.GameOver();
             Debug.Log("Player Collision");
             Time.timeScale = 0;
@@ -30,6 +27,7 @@ public class PlayerCollide : MonoBehaviour
         {
             Debug.LogWarning("powerup");
             Destroy(collision.gameObject);
+            isInvincible = true;
         }
     }
 }
