d55319172f044275fa726cc8f524587d5ba3696a
Tundraswan <peroni014@gmail.com>
2023-12-15 12:29:53 -1000

    another balance
    game is easier but gets harder but not impossible

--- Assets/Scripts/ObstacleManager.cs
+++ Assets/Scripts/ObstacleManager.cs
@@ -8,7 +8,9 @@ public class ObstacleManager : MonoBehaviour
     public Vector2 spawnPos = new Vector2(10, 0);
     private float _lastSpawnedTime;
     public float delay = 1;
-    private float speedincrease = 0.0001f;
+    private float speedincrease = 0.00002f;
+
+    private float timeSinceStart;
     
     // Start is called before the first frame update
     void Start()
@@ -19,14 +21,17 @@ public class ObstacleManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        timeSinceStart += Time.deltaTime;
+        
         //this is the part where you can control how fast the obstacles spawn
         if (_lastSpawnedTime + delay < Time.time)
         {
+            print(delay);
             int obstacleIndex = Random.Range(0, ObstaclePrefabs.Length);
             Instantiate(ObstaclePrefabs[obstacleIndex], spawnPos, Quaternion.identity);
             _lastSpawnedTime = Time.time;
             delay = Random.Range(2, 3f);
         }
-        delay = Mathf.Clamp(delay - (float)Time.timeSinceLevelLoad * speedincrease, 0.35f, 4f);
+        delay = Mathf.Clamp(delay - timeSinceStart * speedincrease, 0.5f, 4f);
     }
 } 
